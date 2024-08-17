6c621f683682092be0ee5c1ab73d0e7ffe170268
nishant-thapa <np05cp4a230120@iic.edu.np>
2024-08-17 22:09:23 +0545

    modified

--- blog_pro/settings.py
+++ blog_pro/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blogApp'
 ]
+
+
+EXTERNAL_APPS=['blogApp']
+INSTALLED_APPS+=EXTERNAL_APPS
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
